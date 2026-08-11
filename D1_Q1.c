714f71be1653719afc3b453835cca51e16a3f2c1
Pranali Mewar <pranalimewar250@gmail.com>
2026-08-11 23:39:45 +0530

    Add C programming coursework

---
+++ D1_Q1.c
@@ -0,0 +1,11 @@
+/*Q1: Write a program to input two numbers and display their sum.*/
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d%d",&a,&b);
+    printf("The sum of the two numbers is:%d",a+b);
+    return 0;
+    
+}
