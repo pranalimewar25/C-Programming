66508868679e008c94639ef3599563f116f24b22
Pranali Mewar <pranalimewar250@gmail.com>
2026-08-12 10:21:25 +0530

    Added D3_Q2

---
+++ D3_Q2.c
@@ -0,0 +1,19 @@
+/*Q6: Write a program to swap two numbers using a third variable.*/
+
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter two numbers:\n");
+    scanf("%d%d",&a,&b);
+    printf("a:%d\n",a);
+    printf("b:%d\n",b);
+    
+    c=a;
+    a=b;
+    b=c;
+    printf("a:%d\n",a);
+    printf("b:%d\n",b);
+    return 0;
+    
+}
+
