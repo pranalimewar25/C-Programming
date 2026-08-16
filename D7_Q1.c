11e9ba36ace226904afa897556f703314ff23675
Pranali Mewar <pranalimewar250@gmail.com>
2026-08-16 20:24:53 +0530

    Update files

--- D7_Q1.c
+++ D7_Q1.c
@@ -5,7 +5,7 @@ int main(){
     int year;
     printf("Enter year");
     scanf("%d",&year);
-    if(year%4==0 & year%100!=0) {
+    if((year%4==0 & year%100!=0) || year%400==0) {
 
         printf("Year is a leap year");
     }  else {
